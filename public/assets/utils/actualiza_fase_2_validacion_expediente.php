30c879dac1f5a01dc768cca478ba9e298a9c0b87
jameslee9935 <jameslee9935@gmail.com>
2024-03-20 07:06:21 +0100

    cambios para adaptar LOS ACTOS ADMINISTRATIVOS de la convo 2024

--- public/assets/utils/actualiza_fase_2_validacion_expediente.php
+++ public/assets/utils/actualiza_fase_2_validacion_expediente.php
@@ -1,25 +1,28 @@
 <?php
 require_once 'conectar_a_bbdd.php';
 
-$id = mysqli_real_escape_string($conn, $_POST["id"]); 
+$id = mysqli_real_escape_string($conn, $_POST["id"]);
 
 $fecha_infor_fav_desf = mysqli_real_escape_string($conn, $_POST["fecha_infor_fav_desf"]);
-$fecha_propuesta_resolucion = mysqli_real_escape_string($conn, $_POST["fecha_propuesta_resolucion"]);
-$fecha_propuesta_resolucion_notif = mysqli_real_escape_string($conn, $_POST["fecha_propuesta_resolucion_notif"]);
-$fecha_resolucion = mysqli_real_escape_string($conn, $_POST["fecha_resolucion"]);
-$fecha_requerimiento = mysqli_real_escape_string($conn, $_POST["fecha_requerimiento"]);
+$fecha_firma_propuesta_resolucion_prov = mysqli_real_escape_string($conn, $_POST["fecha_firma_propuesta_resolucion_prov"]);
+$fecha_not_propuesta_resolucion_prov = mysqli_real_escape_string($conn, $_POST["fecha_not_propuesta_resolucion_prov"]);
+$fecha_firma_propuesta_resolucion_def = mysqli_real_escape_string($conn, $_POST["fecha_firma_propuesta_resolucion_def"]);
+$fecha_not_propuesta_resolucion_def = mysqli_real_escape_string($conn, $_POST["fecha_not_propuesta_resolucion_def"]);
+$fecha_firma_res = mysqli_real_escape_string($conn, $_POST["fecha_firma_res"]);
 $fecha_notificacion_resolucion = mysqli_real_escape_string($conn, $_POST["fecha_notificacion_resolucion"]);
 
 $query = "UPDATE pindust_expediente 
     SET  
     fecha_infor_fav_desf = '" . $fecha_infor_fav_desf ."', 
-    fecha_propuesta_resolucion = '" . $fecha_propuesta_resolucion ."', 
-    fecha_propuesta_resolucion_notif = '" . $fecha_propuesta_resolucion_notif ."', 
-    fecha_resolucion = '" . $fecha_resolucion ."', 
-    fecha_requerimiento = '" . $fecha_requerimiento ."', 
+    fecha_firma_propuesta_resolucion_prov = '" . $fecha_firma_propuesta_resolucion_prov ."', 
+    fecha_not_propuesta_resolucion_prov = '" . $fecha_not_propuesta_resolucion_prov ."', 
+    fecha_firma_propuesta_resolucion_def = '" . $fecha_firma_propuesta_resolucion_def ."', 
+    fecha_not_propuesta_resolucion_def = '" . $fecha_not_propuesta_resolucion_def ."', 
+    fecha_firma_res = '" . $fecha_firma_res ."',
     fecha_notificacion_resolucion = '" . $fecha_notificacion_resolucion ."'
     WHERE  id = " . $id;
 
+/* echo $query; */
 $result = mysqli_query($conn, $query);
 mysqli_close($conn);
 echo $result;
