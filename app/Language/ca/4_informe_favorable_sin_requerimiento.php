d530dd4a9822a7b90967faa232c396df75e9b58f
jameslee9935 <jameslee9935@gmail.com>
2024-04-03 10:34:04 +0200

    cambios para adaptar LOS ACTOS ADMINISTRATIVOS de la convo 2024

--- app/Language/ca/4_informe_favorable_sin_requerimiento.php
+++ app/Language/ca/4_informe_favorable_sin_requerimiento.php
@@ -41,7 +41,7 @@ return [
   '4_intro' => "Informe favorable sobre la sol·licitud de l'ajut de xecs de consultoria per a la competitivitat industrial de les Illes Balears en matèria de digitalització, internacionalització, sostenibilitat i gestió avançada a favor de %SOLICITANTE%, amb NIF %NIF%",
   '4_hechos_tit' => "Fets",
   '4_hechos_1_2' => "<ol>
-                    <li>Mitjançant la Resolució del president de l'Institut d'Innovació Empresarial de les Illes Balears (IDI) %RESPRESIDENTE%, es va aprovar la convocatòria per a l'any %CONVO% de l'ajut de xecs de consultoria per a la competitivitat industrial de les Illes Balears en matèria de digitalització, internacionalització, sostenibilitat i gestió avançada (BOIB núm. %BOIBNUM%)</li><br>.
+                    <li>Mitjançant la Resolució del president de l'Institut d'Innovació Empresarial de les Illes Balears (IDI) %RESPRESIDENTE%, es va aprovar la convocatòria per a l'any %CONVO% de l'ajut de xecs de consultoria per a la competitivitat industrial de les Illes Balears en matèria de digitalització, internacionalització, sostenibilitat i gestió avançada (BOIB núm. %BOIBNUM%).</li><br>
                     <li>En data %FECHAREC%, l'IDI va rebre la sol·licitud de %SOLICITANTE%, amb NIF %NIF% i amb registre d'entrada núm. %NUMREC%, d'un ajut per un import de %IMPORTE% per al %PROGRAMA%.</li><br>",
   '4_hechos_3_m' => "<li>En data %FECHARECM%, l'IDI va rebre documentació per completar la sol·licitud en concepte de millora amb registre d'entrada núm. %REFRECM%.</li><br>", 
   '4_hechos_4_5' => "<li>Una vegada revisada la sol·licitud d'ajut, s'ha comprovat que la sol·licitud es va presentar dins el termini i que compleix amb els requisits establerts en la convocatòria.</li><br>
