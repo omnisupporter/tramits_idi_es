e181518741ffc8f0ebe30b1b7768e4952ff07f54
jameslee9935 <jameslee9935@gmail.com>
2024-04-18 10:06:00 +0200

    cambios para adaptar LOS ACTOS ADMINISTRATIVOS de la convo 2024

--- app/Language/ca/13_propuesta_resolucion_definitiva_desfavorable_sin_requerimiento.php
+++ app/Language/ca/13_propuesta_resolucion_definitiva_desfavorable_sin_requerimiento.php
@@ -40,9 +40,9 @@ return [
   '13_intro' => "Proposta de resolució definitiva de la directora gerent de l'Institut d'Innovació Empresarial de les Illes Balears sobre la denegació d'una subvenció a  %SOLICITANTE% destinada a xecs de consultoria per a la competitivitat industrial de les Illes Balears en matèria de digitalització, internacionalització, sostenibilitat i gestió avançada",
   '13_antecedentes_tit' => "Fets",
   '13_antecedentes_1_2_3' => "<ol>
-            <li>El dia %BOIBFECHA% es va publicar en el BOIB núm. %BOIBNUM% la Resolució del president de l'Institut d'Innovació Empresarial de les Illes Balears (IDI) de %FECHARESPRESIDI% per la qual s'aprova la convocatòria per a l'any %CONVO% d'ajuts de xecs de consultoria per a la competitivitat industrial de les Illes Balears en matèria de digitalització, internacionalització, sostenibilitat i gestió avançada.</li><br>
-            <li>En data %FECHASOL%, %SOLICITANTE%, amb NIF %NIF%, ha presentat una sol·licitud per un import de %IMPORTE%, i documentació adjunta en el marc de l'esmentada resolució de convocatòria, per al %PROGRAMA%.</li><br>
-  			    <li>D'acord amb l'apartat vuitè, paràgraf primer, estableix que les sol·licituds de subvenció s'han de resoldre a mesura que entrin en el registre de l'òrgan competent (rigorós ordre d'entrada), encara que no hagi acabat el termini de presentació, fins que s'exhaureixi el crèdit destinat a la convocatòria o, si escau, fins que es resolguin tots els expedients presentats dins el termini, segons l'article 7.3 de l'Ordre de la consellera de Comerç, Indústria i Energia per la qual s'estableixen les bases reguladores per a la concessió de subvencions en matèria de promoció industrial.</li><br>",
+          <li>El dia %BOIBFECHA% es va publicar en el BOIB núm. %BOIBNUM% la Resolució del president de l'Institut d'Innovació Empresarial de les Illes Balears (IDI) de %FECHARESPRESIDI% per la qual s'aprova la convocatòria per a l'any %CONVO% d'ajuts de xecs de consultoria per a la competitivitat industrial de les Illes Balears en matèria de digitalització, internacionalització, sostenibilitat i gestió avançada.</li><br>
+          <li>En data %FECHASOL%, %SOLICITANTE%, amb NIF %NIF%, ha presentat una sol·licitud per un import de %IMPORTE%, i documentació adjunta en el marc de l'esmentada resolució de convocatòria, per al %PROGRAMA%.</li><br>
+  			  <li>D'acord amb l'apartat vuitè, paràgraf primer, estableix que les sol·licituds de subvenció s'han de resoldre a mesura que entrin en el registre de l'òrgan competent (rigorós ordre d'entrada), encara que no hagi acabat el termini de presentació, fins que s'exhaureixi el crèdit destinat a la convocatòria o, si escau, fins que es resolguin tots els expedients presentats dins el termini, segons l'article 7.3 de l'Ordre de la consellera de Comerç, Indústria i Energia per la qual s'estableixen les bases reguladores per a la concessió de subvencions en matèria de promoció industrial.</li><br>",
   '13_antecedentes_4_m' => "<li>En data %FECHARECM%, l'IDI va rebre documentació  per completar la sol·licitud en concepte de millora amb registre d'entrada núm. %REFRECM%</li></ol><br>",
   '13_antecedentes_5_6_7_8' => "<li>La tècnica de l'IDI ha examinat la sol·licitud presentada per %SOLICITANTE%, i ha comprovat que el sol·licitant no compleix amb els requisits establerts a la convocatòria.</li><br>
 			    <li>En data %FECHAINFORME% s'ha emès informe tècnic desfavorable des de l'IDI sobre la concessió d'ajuts en el marc d'aquesta convocatòria, atès que %TEXTOLIBRE%.</li><br> 
@@ -51,15 +51,15 @@ return [
 
   '13_fundamentosDeDerecho_tit' => "Fonaments de dret",
   '13_fundamentosDeDerechoTxt' => "<ol>
-                          <li>El Decret legislatiu 2/2005, de 28 de desembre, pel qual s'aprova el text refós de la Llei de subvencions (BOIB núm. 196, de 31 de desembre).</li><br>
-                          <li>La Llei 38/2003, de 17 de novembre, general de subvencions (BOE núm. 276, de 18 de novembre).</li><br>
-                          <li>La Llei 39/2015, d'1 d'octubre, dels procediment administratiu comú de les administracions públiques.</li><br>
-                          <li>La Llei 40/2015, d'1 d'octubre, de règim jurídic del Sector Públic.</li><br>
-                          <li>La Llei 3/2003, de 26 de març, de règim jurídic de l'Administració de la Comunitat Autònoma de les Illes Balears.</li><br>
-                          <li>El Pla Estratègic de Subvencions de la Comunitat Autònoma de les Illes Balears per als exercicis 2021-2023, aprovat per l'Acord del Consell de Govern de dia 20 de desembre de 2021 (BOIB núm. 175, de 23 de desembre) i prorrogat fins que s'aprovi i desplegui els efectes el nou Pla Estratègic de Subvencions. En concret està especificat a l'objectiu III.2.2 destinat a impulsar la indústria de les Illes Balears en matèria de digitalització, internacionalització i sostenibilitat.</li><br>
-                          <li>El Decret 6/2013, de 8 de febrer, de mesures de simplificació documental en els procediments administratius (BOIB núm. 21, de 12 de febrer).</li><br>
-                          <li>El Decret 75/2004, de 27 d'agost, pel qual es despleguen determinats aspectes de la Llei de finances i de les lleis de pressuposts generals de la Comunitat Autònoma de les Illes Balears (BOIB núm. 122, de 31 de setembre).</li><br>
-                          <li>L'Ordre de la consellera de Comerç, Indústria i Energia per la qual s'estableixen les bases reguladores per a la concessió de subvencions en matèria de promoció industrial (BOIB núm. 52, de 17 d'abril de 2008).</li></ol>",
+          <li>El Decret legislatiu 2/2005, de 28 de desembre, pel qual s'aprova el text refós de la Llei de subvencions (BOIB núm. 196, de 31 de desembre).</li><br>
+          <li>La Llei 38/2003, de 17 de novembre, general de subvencions (BOE núm. 276, de 18 de novembre).</li><br>
+          <li>La Llei 39/2015, d'1 d'octubre, dels procediment administratiu comú de les administracions públiques.</li><br>
+          <li>La Llei 40/2015, d'1 d'octubre, de règim jurídic del Sector Públic.</li><br>
+          <li>La Llei 3/2003, de 26 de març, de règim jurídic de l'Administració de la Comunitat Autònoma de les Illes Balears.</li><br>
+          <li>El Pla Estratègic de Subvencions de la Comunitat Autònoma de les Illes Balears per als exercicis 2021-2023, aprovat per l'Acord del Consell de Govern de dia 20 de desembre de 2021 (BOIB núm. 175, de 23 de desembre) i prorrogat fins que s'aprovi i desplegui els efectes el nou Pla Estratègic de Subvencions. En concret està especificat a l'objectiu III.2.2 destinat a impulsar la indústria de les Illes Balears en matèria de digitalització, internacionalització i sostenibilitat.</li><br>
+          <li>El Decret 6/2013, de 8 de febrer, de mesures de simplificació documental en els procediments administratius (BOIB núm. 21, de 12 de febrer).</li><br>
+          <li>El Decret 75/2004, de 27 d'agost, pel qual es despleguen determinats aspectes de la Llei de finances i de les lleis de pressuposts generals de la Comunitat Autònoma de les Illes Balears (BOIB núm. 122, de 31 de setembre).</li><br>
+          <li>L'Ordre de la consellera de Comerç, Indústria i Energia per la qual s'estableixen les bases reguladores per a la concessió de subvencions en matèria de promoció industrial (BOIB núm. 52, de 17 d'abril de 2008).</li></ol>",
   
   '13_propuestaresoluciondef_tit' => "Proposta de resolució definitiva",
 
