a737d724b57b04942e9f6905fbc3b4375ff4144e
jameslee9935 <jameslee9935@gmail.com>
2024-08-21 07:24:21 +0200

    he creado plantilla acto admin 12

---
+++ app/Language/ca/isba_12_informe_inicio_requerimiento_justificacion.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Database language strings.
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ * Copyright (c) 2019-2023 CodeIgniter Foundation & ADR Balears
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team & ADR Balears
+ * @copyright  2019-2023 CodeIgniter Foundation & ADR Balears
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+  'intro' => "Informe d'inici de requeriment d'esmena de la documentació justificativa d'una subvenció a la micro empresa, petita i mitjana empresa de caràcter industrial de les Illes Balears destinada a cobrir les despeses financeres d'operacions de finançament d'inversions productives a favor de %SOLICITANTE%, amb NIF %NIF%",
+  'p1' => "El %FECHA_RESOL_CONCE%, es va concedir l'ajut a favor de %SOLICITANTE% amb NIF %NIF% per valor de %IMPORTEAYUDA% , en el marc de la convocatòria de subvencions per a l'any 2024-25 de l'ajut de a la micro empresa, petita i mitjana empresa de caràcter industrial de les Illes Balears destinada a cobrir les despeses financeres d'operacions de finançament d'inversions productives (BOIB Núm. XX, %BOIBNUM%).",
+  'p2' => "El %FECHA_REUNION_CIERRE%, el/la tècnic/a de l'ADR, el/la consultor/a i el/la beneficiari/a varen realitzar la reunió de tancament.  En conseqüència, el %FECHA_LIMITE_ALEGACIONES% era la data màxima per justificar la subvenció.",
+  'p3' => "Una vegada acabat aquest termini i revisat l'expedient es pot constatar que  %TEXTO_LIBRE%",
+  'conclusion_tit' => "Conclusió",
+  'conclusionTxt'  => "Per tot això, propòs fer un requeriment d'esmena de la documentació justificativa de la subvenció concedida.",
+  'firma' => "Palma, en data de la signatura electrònica<br><br>El/la tècnic/a"
+];
