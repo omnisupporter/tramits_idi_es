a62023d26367a4ae5147921ac636884477bbf979
jameslee9935 <jameslee9935@gmail.com>
2024-09-04 19:13:08 +0200

    he añadido la fecha de firma o rechazo del acto administrativo

--- app/Views/pages/forms/modDocs/IDI-ISBA/propuesta-resolucion-revocacion-por-no-justificar.php
+++ app/Views/pages/forms/modDocs/IDI-ISBA/propuesta-resolucion-revocacion-por-no-justificar.php
@@ -27,11 +27,13 @@
 						break;
 						case 'REJECTED':
 						$estado_firma = "<a href=".base_url('public/index.php/expedientes/muestrasolicitudrechazada/'.$requestPublicAccessId)."><div class = 'btn btn-warning btn-acto-admin'><i class='fa fa-warning'></i> Signatura rebutjada</div>";
-						$estado_firma .= "</a>";				
+						$estado_firma .= "</a>";
+						$estado_firma .= gmdate("d-m-Y", intval ($respuesta['rejectInfo']['rejectDate']/1000));			
 						break;
 						case 'COMPLETED':
 						$estado_firma = "<a href=".base_url('public/index.php/expedientes/muestrasolicitudfirmada/'.$requestPublicAccessId)." ><div class='btn btn-success btn-acto-admin'><i class='fa fa-check'></i> Signat</div>";		
-						$estado_firma .= "</a>";					
+						$estado_firma .= "</a>";
+						$estado_firma .= gmdate("d-m-Y", intval ($respuesta['endDate']/1000));					
 						break;
 						case 'IN_PROCESS':
 						$estado_firma = "<a href=".base_url('public/index.php/expedientes/muestrasolicitudfirmada/'.$requestPublicAccessId)." ><div class='btn btn-secondary btn-acto-admin'><i class='fa fa-check'></i> En curs</div>";		
