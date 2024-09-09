992aa8419ae7803029a30215d81b0df7847ec0a5
jameslee9935 <jameslee9935@gmail.com>
2024-09-09 08:49:48 +0200

    he actualizado la edición expediente ISBA

--- public/assets/js/edita-expediente-isba.js
+++ public/assets/js/edita-expediente-isba.js
@@ -388,8 +388,8 @@ function actualiza_fase_4_justificacion_expediente_idi_isba(formName) {  //SE EM
 		return;
 	}
 	let id = document.getElementById("id").value;
-	//let fecha_res_liquidacion = document.getElementById("fecha_res_liquidacion").value; // Data SEU justificació
-	let fecha_limite_justificacion = document.getElementById("fecha_limite_justificacion").value; // Referència SEU justificació
+	let fecha_REC_justificacion = document.getElementById("fecha_REC_justificacion").value; // Data SEU justificació
+	let ref_REC_justificacion = document.getElementById("ref_REC_justificacion").value; // Data SEU justificació
 	let fecha_not_res_pago = document.getElementById("fecha_not_res_pago").value; // Data notificació resolucio de pagament
 	let fecha_firma_requerimiento_justificacion = document.getElementById("fecha_firma_requerimiento_justificacion").value; // Data firma requeriment justificació
 	let fecha_not_req_just = document.getElementById("fecha_not_req_just").value // Data notificació requeriment justificació
@@ -397,8 +397,9 @@ function actualiza_fase_4_justificacion_expediente_idi_isba(formName) {  //SE EM
 	let ref_REC_requerimiento_justificacion = document.getElementById("ref_REC_requerimiento_justificacion").value; // Referència REC requeriment justificació
 	let fecha_propuesta_rev = document.getElementById("fecha_propuesta_rev").value; // Data propuesta revocació
 	let fecha_resolucion_rev = document.getElementById("fecha_resolucion_rev").value; // Data resolución revocació
-	
-	for (let step = 0; step < 8; step++) {
+	let fecha_firma_res_pago_just = document.getElementById("fecha_firma_res_pago_just").value; // Firma resolució de pagament i justificació
+
+	for (let step = 0; step < 12; step++) {
 		document.getElementsByClassName("form-group justificacion")[step].style.opacity = "0.1";
 	}
 
@@ -408,7 +409,10 @@ function actualiza_fase_4_justificacion_expediente_idi_isba(formName) {  //SE EM
 	send_fase_4.disabled = true;
 	$.post(
 		"/public/assets/utils/actualiza_fase_4_justificacion_expediente_isba.php",
-		{ id: id, fecha_limite_justificacion: fecha_limite_justificacion, 
+		{ id: id, 
+			fecha_REC_justificacion: fecha_REC_justificacion, 
+			ref_REC_justificacion: ref_REC_justificacion,
+			fecha_firma_res_pago_just: fecha_firma_res_pago_just,
 			fecha_not_res_pago: fecha_not_res_pago, 
 			fecha_firma_requerimiento_justificacion: fecha_firma_requerimiento_justificacion,
 			fecha_not_req_just: fecha_not_req_just,
@@ -425,7 +429,7 @@ function actualiza_fase_4_justificacion_expediente_idi_isba(formName) {  //SE EM
 				send_fase_4.className = "btn-itramits btn-success-itramits";
 				send_fase_4.disabled = false;
 			}
-			for (let step = 0; step < 8; step++) {
+			for (let step = 0; step < 12; step++) {
 				document.getElementsByClassName("form-group justificacion")[step].style.opacity = "1.0";
 			}			
 		}
--- public/assets/utils/actualiza_fase_4_justificacion_expediente_isba.php
+++ public/assets/utils/actualiza_fase_4_justificacion_expediente_isba.php
@@ -2,8 +2,9 @@
 require_once 'conectar_a_bbdd.php';
 
 $id = mysqli_real_escape_string($conn, $_POST["id"]); 
-/* $fecha_res_liquidacion = mysqli_real_escape_string($conn, $_POST["fecha_res_liquidacion"]); */
-$fecha_limite_justificacion = mysqli_real_escape_string($conn, $_POST["fecha_limite_justificacion"]);
+$fecha_REC_justificacion = mysqli_real_escape_string($conn, $_POST["fecha_REC_justificacion"]);
+$ref_REC_justificacion = mysqli_real_escape_string($conn, $_POST["ref_REC_justificacion"]);
+$fecha_firma_res_pago_just =  mysqli_real_escape_string($conn, $_POST["fecha_firma_res_pago_just"]);
 $fecha_not_res_pago = mysqli_real_escape_string($conn, $_POST["fecha_not_res_pago"]);
 $fecha_firma_requerimiento_justificacion = mysqli_real_escape_string($conn, $_POST["fecha_firma_requerimiento_justificacion"]);
 $fecha_not_req_just = mysqli_real_escape_string($conn, $_POST["fecha_not_req_just"]);
@@ -20,18 +21,28 @@ $fecha_resolucion_rev = mysqli_real_escape_string($conn, $_POST["fecha_resolucio
     $fecha_fecha_res_liquidacion = explode("/", $fecha_fecha_res_liquidacion);
     $fecha_res_liquidacion = $fecha_fecha_res_liquidacion[2]."-".$fecha_fecha_res_liquidacion[1]."-".$fecha_fecha_res_liquidacion[0] ." ".$hora_REC_justificacion;
 } */
+
+if (strpos($fecha_REC_justificacion, "/") > 0) {
+    $fecha_REC_justificacion = explode(" ", $fecha_REC_justificacion);
+    $hora_REC_requerimiento_justificacion = $fecha_REC_justificacion[1];
+    $fecha_fecha_REC_justificacion = $fecha_REC_justificacion[0];
+    $fecha_fecha_REC_justificacion = explode("/", $fecha_fecha_REC_justificacion);
+    $fecha_REC_justificacion = $fecha_fecha_REC_justificacion[2]."-".$fecha_fecha_REC_justificacion[1]."-".$fecha_fecha_REC_justificacion[0] ." ".$hora_REC_requerimiento_justificacion;
+}
+if ( strlen($fecha_REC_justificacion) != 0) {
+    $date_REC_requerimiento_justificacion = date_format(date_create($fecha_REC_justificacion), "Y-m-d H:i:s");
+} else {
+    $date_REC_requerimiento_justificacion = "";
+}
+
+
+
 if (strpos($fecha_REC_requerimiento_justificacion, "/") > 0) {
     $fecha_REC_requerimiento_justificacion = explode(" ", $fecha_REC_requerimiento_justificacion);
     $hora_REC_requerimiento_justificacion = $fecha_REC_requerimiento_justificacion[1];
     $fecha_fecha_REC_requerimiento_justificacion = $fecha_REC_requerimiento_justificacion[0];
     $fecha_fecha_REC_requerimiento_justificacion = explode("/", $fecha_fecha_REC_requerimiento_justificacion);
     $fecha_REC_requerimiento_justificacion = $fecha_fecha_REC_requerimiento_justificacion[2]."-".$fecha_fecha_REC_requerimiento_justificacion[1]."-".$fecha_fecha_REC_requerimiento_justificacion[0] ." ".$hora_REC_requerimiento_justificacion;
-}
-
-if ( strlen($fecha_limite_justificacion) != 0) {
-    $fecha_limite_justificacion = date_format(date_create($fecha_limite_justificacion), "Y-m-d H:i:s");
-} else {
-    $fecha_limite_justificacion = "";
 }
 
 if ( strlen($fecha_REC_requerimiento_justificacion) != 0) {
@@ -48,8 +59,9 @@ if ( (strlen($date_REC_requerimiento_justificacion) != 0) && ($date_REC_requerim
 
 $query = "UPDATE pindust_expediente 
     SET  
-    /* fecha_res_liquidacion = '" . $fecha_res_liquidacion ."', */
-    fecha_limite_justificacion = '" . mb_strtoupper($fecha_limite_justificacion) ."',
+    fecha_REC_justificacion = '" . $date_REC_requerimiento_justificacion  ."',
+    ref_REC_justificacion = '" . $ref_REC_justificacion  ."',
+    fecha_firma_res_pago_just = '" . $fecha_firma_res_pago_just  ."',
     fecha_not_res_pago  = '" . $fecha_not_res_pago  ."',
     fecha_firma_requerimiento_justificacion = '" . $fecha_firma_requerimiento_justificacion ."',
     fecha_not_req_just = '" . $fecha_not_req_just ."',
