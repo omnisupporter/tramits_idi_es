6fa76534ee2d3913eec750a89af736ce8f555922
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-11-19 13:33:13 +0100

    creando actos administrativos para renovación ILS

---
+++ app/Language/ca/ILS_12_renovacion_informe_desfavorable_con_req.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Database language strings.
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ * Copyright (c) 2019-2023 CodeIgniter Foundation & ADR Balears
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team & ADR Balears
+ * @copyright  2019-2023 CodeIgniter Foundation & ADR Balears
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+  'intro' => "Informe desfavorable sobre la renovació d'adhesió a la marca Indústria Local Sostenible (ILS) a favor de  %SOLICITANTE%, amb NIF %NIF%",
+  'hechos_tit' => "Fets",
+  'hechos_1_2_3_4' => "<ol>
+    <li>La  Marca de Garantia núm.4113843 ILS INDÚSTRIA LOCAL SOSTENIBLE (figurativa), en classes 35 i 42, va ser aprovada per l'Oficina Espanyola de Patents i Marques el 20 de desembre de 2021 i publicada la F. de conces-sió el 28 de desembre de 2021.</li><br>
+    <li>En data %FECHARES%, el President de l'IDI (ara ADR Balears) va emetre resolució de concessió a favor de  %SOLICITANTE%, amb NIF %NIF%.</li><br>
+    <li>En data %FECHANOTIFREQU% , es va notificar un requeriment a l'empresa %SOLICITANTE%, amb NIF %NIF% per completar la documentació per a la renovació de la marca.</li><br>
+    <li>Una vegada transcorreguts 10 dies, s'ha comprovat que no s'ha enviat la documentació requerida o que la que s'ha enviat no compleix amb els re-quisits establerts en el reglament de marca.</li><br>",
+  'conclusion_tit' => "<b>Conclusió</b>",
+  'conclusionTxt'  => "Una vegada revisada la documentació de renovació de marca, s'ha comprovat que l'empresa %SOLICITANTE%, amb NIF %NIF% no compleix amb els requisits de renovació establerts en el Reglament de la Marca de Garantia ILS, propòs al president de l'ADR Balears que dicti una resolució de denegació de renovació de la marca Indústria Local Sostenible.",
+  'firma' => "Palma, en data de la signatura electrònica<br><br>El/la tècnic/a"
+];
